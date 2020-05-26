a12d4b0d71c9671e31be20e3e12293a890630e66
aaronchoi5 <aaron.choi8@gmail.com>
2020-05-26 19:50:18 -0400

    pulled out cards and added pile class

---
+++ cards.py
@@ -0,0 +1,21 @@
+import random
+import uuid
+class Card:
+    def __init__(self, value, color):
+        self.value = value
+        self.suit = color
+
+class Deck:
+	def __init__(self):
+		self.cards = []
+		colors = ['heart', 'spades']
+		for c in colors:
+			for i in range(15):
+				self.cards.append(Card(i, c))
+
+	def shuffle(self):
+		random.shuffle(self.cards)
+class Pile:
+	def __init__(self):
+		self.cards = []
+		self.id = uuid.uuid4()
--- platoon.py
+++ platoon.py
@@ -1,30 +1,18 @@
 import random
-class Card:
-    def __init__(self, value, color):
-        self.value = value
-        self.suit = color
-
-class Deck:
-	def __init__(self):
-		self.cards = []
-		colors = ['heart', 'spades']
-		for c in colors:
-			for i in range(14):
-				self.cards.append(Card(i, c))
-
-	def shuffle(self):
-		random.shuffle(self.cards)
+import cards
 
 class Player:
 	def __init__(self, cards):
 		self.cards = cards
 		self.firstPlayer = False
-		self.pile1 = []
-		self.pile2 = []
-		self.pile3 = []
-		self.pile4 = []
-		self.pile5 = []
+		self.pile1 = Pile()
+		self.pile2 = Pile()
+		self.pile3 = Pile()
+		self.pile4 = Pile()
+		self.pile5 = Pile()
 		#guid for security?
+	def getCards():
+		return self.cards
 
 class Game:
 	def __init__(self):
@@ -33,6 +21,28 @@ class Game:
 		self.playerA = Player(self.deck.cards[:10])
 		self.playerB = Player(self.deck.cards[10:20])
 		self.deck.cards = self.deck.cards[20:]
+
+	def start(numOfRounds):
+		for i in numOfRounds:
+			self.deck = Deck()
+			self.deck.shuffle()
+			self.playerA = Player(self.deck.cards[:10])
+			self.playerB = Player(self.deck.cards[10:20])
+			self.deck.cards = self.deck.cards[20:]
+
+			r = Round()
+			#get requests to get player's cards so we can display them on screen
+			r.assignCardsToPiles()
+
+			r.determineFirst()
+			turn = 0 if self.playerA.firstPlayer else 1
+
+			for trickNum in range(5):
+				if turn % 2 == 0:
+					#playerA fights FE provides guid for both
+				else:
+					#playerB fights
+				turn += 1
 
 	class Round:
 		def determineFirst(self):
@@ -50,28 +60,35 @@ class Game:
 		def assignCardsToPiles(pile1, pile2, pile3, pile4, pile5):
 			#post request most likely id part of route?
 			if True:
-				self.playerA.pile1 = pile1
-				self.playerA.pile2 = pile2
-				self.playerA.pile3 = pile3
-				self.playerA.pile4 = pile4
-				self.playerA.pile5 = pile5
+				self.playerA.pile1.cards = pile1
+				self.playerA.pile2.cards = pile2
+				self.playerA.pile3.cards = pile3
+				self.playerA.pile4.cards = pile4
+				self.playerA.pile5.cards = pile5
 				self.playerA.cards = []
 			else:
-				self.playerB.pile1 = pile1
-				self.playerB.pile2 = pile2
-				self.playerB.pile3 = pile3
-				self.playerB.pile4 = pile4
-				self.playerB.pile5 = pile5
+				self.playerB.pile1.cards = pile1
+				self.playerB.pile2.cards = pile2
+				self.playerB.pile3.cards = pile3
+				self.playerB.pile4.cards = pile4
+				self.playerB.pile5.cards = pile5
 				self.playerB.cards = []
-				
+
 			#return true
+		def fight(pileA, pileB):
+			#TODO do i pass in pile id's?
+			#check for specials in pile A and pileB 
+			#13 should be wizard, 12 should be king, 11 should be Queen, 10 = Jack, 9 = 10, ...,  0 = ace
+			#resolve wizard
+			#if 13 in pileA:  pileA, pileB = pileB, pileA
+			#if 13 in pileB:  pileA, pileB = pileB, pileA
+			#if 12 in both check for higher values
+			#if 12 in pileA: checkForAce if ace in PileB B wins vice versa
+			#if 12 in pileB: as above
+			#if ace in both check for higher values if none than it's a tie
+			#if ace in one or the other it loses
+			#total up cards in each pile and see which one is higher
 
-
-		class Trick:
-			def __init__(self):
-
-
-		
 
 g = Game()
 
